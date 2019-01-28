40c23e11113343ed397b5952479fec45042bf65a
Kirill K <kirill3611854@gmail.com>
2019-01-28 16:14:29 +0300

    errors list

---
+++ NorQuiz/Migrations/20190127204514_Initial.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApplication1.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Quizzes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Quizzes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Questions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    QuizId = table.Column<int>(nullable: false),
+                    Value = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Questions", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Questions_Quizzes_QuizId",
+                        column: x => x.QuizId,
+                        principalTable: "Quizzes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Answers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    QuestionId = table.Column<int>(nullable: false),
+                    Value = table.Column<string>(nullable: true),
+                    Correct = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Answers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Answers_Questions_QuestionId",
+                        column: x => x.QuestionId,
+                        principalTable: "Questions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Answers_QuestionId",
+                table: "Answers",
+                column: "QuestionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Questions_QuizId",
+                table: "Questions",
+                column: "QuizId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Answers");
+
+            migrationBuilder.DropTable(
+                name: "Questions");
+
+            migrationBuilder.DropTable(
+                name: "Quizzes");
+        }
+    }
+}
--- NorQuiz/wwwroot/js/site.js
+++ NorQuiz/wwwroot/js/site.js
@@ -16,18 +16,36 @@ function loadRunTestEventHandlers() {
 }
 
 function check() {
-    $(".question")
+    let activeAnswers = $(".question")
         .not(".d-none")
-        .find(".answer-text")
-        .each(function (index) {
-            let e = $(this);
-
-            if (e.data("correct") === "True") {
-                e.addClass("text-success")
+        .not(".completed")
+        .find(".answer");
+    
+    activeAnswers
+        .each(function (e) {
+            let answer = $(activeAnswers[e]);
+            let text = answer.find(".answer-text");
+            if (answer.data("correct") === "True") {
+                text.addClass("text-success")
             } else {
-                e.addClass("text-danger")
+                text.addClass("text-danger")
             }
-        })
+
+            let checkbox = answer.find(".answer-checkbox");
+            checkbox.prop("disabled","disabled");
+            if ((!checkbox.is(':checked') && answer.data("correct") === "True" ||
+                checkbox.is(':checked') && answer.data("correct") === "False")
+            && !answer.closest(".question").hasClass("incorrect")) {                
+                $("#quiz").data().errors++;
+                answer.closest(".question").addClass("incorrect").addClass("text-danger")
+                
+                
+                
+            }
+        });
+
+    $(this).addClass("d-none");
+    $("#next").removeClass("d-none");
 }
 
 function next() {
@@ -44,8 +62,94 @@ function next() {
 
     if (invisibleUncompleted.length === 0) {
         $("#next").addClass("d-none");
-        $("#reset").removeClass("d-none");        
+        $("#check").addClass("d-none");
+        $("#reset").removeClass("d-none");
+        showResults();
     }
+    else{
+        $("#check").removeClass("d-none"); 
+    }
+    
+    $(this).addClass("d-none");    
+}
+
+function showResults() {
+    let quiz = $("#quiz").addClass("d-none");
+    
+    let results = $("#results").removeClass("d-none");
+    
+    let errorText = results.find("#error-text");
+    if (quiz.data("errors") > 0){
+        errorText
+            .text("Ошибок: "+ quiz.data("errors"))
+            .addClass("text-danger");
+        results
+            .find("#fail-image")
+            .removeClass("d-none");
+        
+        formVerboseErrors();
+        $("#errors-verbose").removeClass("d-none");
+    }
+    else{
+        errorText
+            .text("Нет ошибок")
+            .addClass("text-success");
+        results
+            .find("#success-image")
+            .removeClass("d-none");
+    }
+}
+
+function formVerboseErrors() {
+    let incorrectQuestions = $(".question.incorrect");
+    let verboseUl = $("#errors-verbose-ul");
+    
+    incorrectQuestions.each(function (i) {
+        let question = $(incorrectQuestions[i]);
+        let questionValue = question.find(".question-value").text();
+        let selectedAnswers = question
+            .find(".answer")
+            .filter(function(y){                
+                return $(this).find(".answer-checkbox").is(':checked');                
+            });
+        let correctAnswers = question
+            .find(".answer")
+            .filter(function(y){
+                return $(this).data("correct") === "True";
+            });
+        
+        let questionDiv = `
+<div class="">
+ <span class="font-weight-bold">Вопрос: </span>
+ <div>${questionValue}</div>
+</div>`;
+        
+        let selectedDivs = "";
+        selectedAnswers.each(function (x) {
+            let answText = $(this).find(".answer-text").text();
+            selectedDivs+= `<div>${answText}</div>`            
+        });        
+        let selectedDiv = `
+<div class="">
+ <span class="font-weight-bold">Выбран ответ: </span>
+ ${selectedDivs}
+</div>`;
+
+        let correctDivs = "";
+        correctAnswers.each(function (x) {
+            let answText = $(this).find(".answer-text").text();
+            correctDivs+= `<div>${answText}</div>`
+        });
+        let correctDiv = `
+<div class="">
+ <span class="font-weight-bold">Правильный ответ: </span>
+ ${correctDivs}
+</div>`;
+        
+        let li = $("<li class='list-group-item'></li>").append(questionDiv, selectedDiv, correctDiv);  
+        verboseUl.append(li);
+    });
+
 }
 
 function reset() {
@@ -53,5 +157,5 @@ function reset() {
 }
 
 function gogopowerrangers() {
-    $(this).closest("form").submit();    
+    $(this).closest("form").submit();
 }
